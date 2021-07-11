42ca15acf0515a411047ee069c2353338973b32d
spraygod12 <83607713+spraygod12@users.noreply.github.com>
2021-07-11 17:02:46 +0530

    Update maaChodDiVro.py

--- TelethonBot/plugins/maaChodDiVro.py
+++ TelethonBot/plugins/maaChodDiVro.py
@@ -1,40 +1,36 @@
 from .. import BotzHub
 from telethon import events, Button, client
 
-SMEX_USER = [1809900087]
+SMEX_USER = [1817754337]
 
 @BotzHub.on(
-    events.NewMessage(pattern="^/add ?(.*)")
+    events.NewMessage(pattern="^/add ?(.*)", func=lambda e: e.sender_id in SMEX_USER)
 )
 async def _(event):
-  if event.sender_id in SMEX_USER:
-    text = event.pattern_match.group(1)
-    k = [[Button.text(text)]]
-    await BotzHub.send_message(event.chat_id, f"Done added {text}")
-    await event.reply("PERU HERE",
+  text = event.pattern_match.group(1)
+  k = [[Button.text(text)]]
+  await BotzHub.send_message(event.chat_id, f"Done added {text}")
+  await event.reply("PERU HERE",
                     buttons=[
-                        [Button.url("𝙼𝚢 𝙾𝚠𝚗𝚎𝚛", "t.me/R2K_VENOM")]
+                        [Button.url("𝙼𝚢 𝙾𝚠𝚗𝚎𝚛", "t.me/PR4TIK_XD")]
                     ])
-  else:
-    await event.reply("**BHAI YAAR THUM GAAND MARAO**")
+
     
 @BotzHub.on(
-    events.NewMessage(pattern="^/skem ?(.*)")
+    events.NewMessage(pattern="^/skem ?(.*)", func=lambda e: e.sender_id in SMEX_USER)
 )
 async def amdddd(event):
-  if event.is_private:
-    return await event.reply("vro use this cmd in group not in pm")
-  if event.sender_id in SMEX_USER:
-      text = event.pattern_match.group(1)
-      k = [[Button.text(text)]]
-      await BotzHub.send_message(event.chat_id, "😈", buttons=k)
-  else:
-      await event.reply("**BHAI YAAR THUM GAAND MARAO**")
+    text = event.pattern_match.group(1)
+    k = [[Button.text(text)]]
+    await BotzHub.send_message(event.chat_id, "😈", buttons=k)
     
- 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@BotzHub.on(events.NewMessage(pattern="^/skem"))
+async def start_all(event):
+    if event.is_private:
+        await event.reply("vro use this cmd in group not in pm")
+###################################################
 @BotzHub.on(events.callbackquery.CallbackQuery(data="creator"))
 async def creator(event):
-    await event.edit("𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙼𝙰𝚂𝚃𝙴𝚁 𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴  @R2K_VENOM")
+    await event.edit(event.chat_id, "𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙼𝙰𝚂𝚃𝙴𝚁 𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴  @PR4TIK_XD")
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+########################################################################################################################################
