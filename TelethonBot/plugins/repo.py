69489ff9943f17727600d35bae1789d0034cfd3d
R2K-VENOM <84581636+R2K-VENOM@users.noreply.github.com>
2021-07-11 12:20:35 +0530

    Update repo.py

--- TelethonBot/plugins/repo.py
+++ TelethonBot/plugins/repo.py
@@ -6,6 +6,6 @@ SMEX_USER = [1809900087]
 @BotzHub.on(events.NewMessage(pattern="^/repo ?(.*)"))
 async def _(event):
   if event.sender_id in SMEX_USER:
-    await BotzHub.send_message("**HEY MASTER HERE IS YOUR REPO**\n\nhttps://github.com/R2K-VENOM/TelethonBot")
+    await BotzHub.send_message(event.chat_id, "**HEY MASTER HERE IS YOUR REPO**\n\nhttps://github.com/R2K-VENOM/TelethonBot")
   else:
     await event.reply("**BHAI YAAR THUM GAAND MARAO**")
